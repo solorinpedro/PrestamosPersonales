18a4824f0bdeb98dba63e5ed6e16cb0f93aea2a2
Pedro Luis Solorin <psolorin@gmail.com>
2022-09-06 14:15:17 -0400

    Initial commit

---
+++ app/src/main/java/edu/ucne/prestamospersonales/view/ocupacionviewmodel.kt
@@ -0,0 +1,32 @@
+package edu.ucne.prestamospersonales.view
+import androidx.compose.runtime.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.ucne.prestamospersonales.data.repository.Ocupacionrepository
+import edu.ucne.prestamospersonales.model.Ocupacion
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ocupacionviewmodel @Inject constructor(
+    val ocupacionrepository: Ocupacionrepository
+    ):ViewModel(){
+
+    var descripcion by mutableStateOf("")
+    var salario by mutableStateOf("")
+    fun Guardar(){
+        viewModelScope.launch {
+            ocupacionrepository.Insertar(
+                Ocupacion(
+                  ocupacionId = 0,
+                    descripcion = descripcion,
+                    salario = salario.toFloat()
+                )
+            )
+        }
+    }
+}
+
+
