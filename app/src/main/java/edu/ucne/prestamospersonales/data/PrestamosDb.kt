b208ddb9c3a3352966273952a1774df89885f73e
Pedro Luis Solorin <psolorin@gmail.com>
2022-09-13 16:16:42 -0400

    Nueva entidad Persona

--- app/src/main/java/edu/ucne/prestamospersonales/data/PrestamosDb.kt
+++ app/src/main/java/edu/ucne/prestamospersonales/data/PrestamosDb.kt
@@ -3,11 +3,14 @@ package edu.ucne.prestamospersonales.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.prestamospersonales.model.Ocupacion
+import edu.ucne.prestamospersonales.model.Persona
 
 @Database(
-    entities = [Ocupacion::class],
-    version =1
+    entities = [Ocupacion::class, Persona::class],
+    version = 2
 )
 abstract class PrestamosDb: RoomDatabase(){
     abstract val OcupacionDao: OcupacionDao
+
+    abstract val PersonasDao: PersonasDao
 }
